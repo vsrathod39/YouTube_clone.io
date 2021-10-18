39679e26d0ad692438e8e2207c31d32a638304b9
Vikas Kumar <vsrathod39@gmail.com>
2021-10-18 22:54:11 +0530

    Signup/Login authentication implemented

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ var globalData;
 
 async function ytAPICall(){
     let query = document.getElementById("searchKeyword").value;
-    // https://api.themoviedb.org/3/search/movie?&api_key=1229e943aec051105219f4ea7a80c817${"&query="}${query}
 
     // https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA2eQDFN-PFovXa9BWhv9BorJ_DL9fPKbI&maxResults=20
 
-    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyDppDIBLwWIeHL6RICPfthgiBrxo5AQMKE&maxResults=20&safeSearch=strict&videoCaption=closedCaption&part=snippet&chart=mostPopular&regionCode=IN`);
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA2eQDFN-PFovXa9BWhv9BorJ_DL9fPKbI&maxResults=20&safeSearch=strict&videoCaption=closedCaption&part=snippet&chart=mostPopular&regionCode=IN`);
 
     let dataFetched = await res.json();
     globalData = dataFetched.items
@@ -22,7 +21,6 @@ async function ytAPICall(){
 
 let timerId;
 function debounce(keyword, delay){
-    // console.log(keyword, delay);
 
     if(timerId){
         clearTimeout(timerId);
@@ -44,8 +42,6 @@ async function appendSearchedResult(keyword){
     searchedResult.innerHTML = null;
 
     let res = await ytAPICall(keyword);
-    // showVideoInBody(res);
-    // console.log(res)
 
     res.forEach(({snippet:{title}, videoElement}) => {
         let div =  document.createElement("div");
@@ -75,7 +71,6 @@ function showSearchedResultVideo(){
     globalData.forEach(({id:{videoId}, snippet:{title}, snippet:{channelTitle}}) =>{
         let div = document.createElement("div");
         div.setAttribute("class", "videoAPIBoxSecoundry");
-        // console.log(id, snippet);
 
         let vidDiv = document.createElement("div");
         vidDiv.innerHTML = `<iframe src=https://www.youtube.com/embed/${videoId} title="YouTube video" frameBorder="0" width="380" height="220" allow="fullscreen"></iframe>`;
@@ -107,7 +102,7 @@ function showSearchedResultVideo(){
 
 async function mostPopularVideo(){
     // 
-    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?type=video&key=AIzaSyDppDIBLwWIeHL6RICPfthgiBrxo5AQMKE&maxResults=20&safeSearch=strict&videoCaption=closedCaption&part=snippet&chart=mostPopular&regionCode=IN`);
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?type=video&key=AIzaSyA2eQDFN-PFovXa9BWhv9BorJ_DL9fPKbI&maxResults=20&safeSearch=strict&videoCaption=closedCaption&part=snippet&chart=mostPopular&regionCode=IN`);
 
     let data = await res.json();
     showVideoInBody(data.items);
@@ -121,7 +116,6 @@ function showVideoInBody(data){
     data.forEach( ({id:{videoId}, snippet:{title}, snippet:{channelTitle}}) => {
         let div = document.createElement("div");
         div.setAttribute("class", "videoAPIBox")
-        // console.log(id, snippet);
 
         let vidDiv = document.createElement("div");
         vidDiv.innerHTML = `<iframe src=https://www.youtube.com/embed/${videoId} title="YouTube video" frameBorder="0" width="280" height="200" allow="fullscreen"></iframe>`;
@@ -147,3 +141,299 @@ function showVideoInBody(data){
 }
 
 mostPopularVideo();
+
+// -------------------------------------------------------------------------------------
+// Registration/Signup form
+function loginPopup(){
+    let parrent = document.getElementById("mainContainer");
+    // parrent.innerHTML = null;
+    if(document.getElementById("loginForm") == null){
+        var div = document.createElement("div");
+        div.setAttribute("id", "loginForm");
+    }else{
+        var div = document.getElementById("loginForm");
+        div.innerHTML = null;
+    }
+
+    let img = document.createElement("img");
+    img.src = "https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/35/000000/external-close-banking-and-finance-kiranshastry-gradient-kiranshastry.png";
+    img.style.cursor = "pointer";
+    img.onclick = function(){
+        removeActive();
+    }
+    div.append(img);
+
+    let heading = document.createElement("h3");
+    heading.textContent = "Sign-up";
+    heading.style.textAlign = "center";
+    heading.style.margin = "10px 0 0 0";
+    div.append(heading);
+
+    let form = document.createElement("form");
+    form.setAttribute("id", "myLogInForm");
+    form.onsubmit = function(){
+        register(event);
+    }
+
+    let ip1 = document.createElement("input");
+    ip1.type = "text";
+    ip1.setAttribute("id", "name");
+    ip1.placeholder = "Name";
+
+    let ip2 = document.createElement("input");
+    ip2.type = "text";
+    ip2.setAttribute("id", "email");
+    ip2.placeholder = "Email";
+
+    let ip3 = document.createElement("input");
+    ip3.type = "text";
+    ip3.setAttribute("id", "password");
+    ip3.placeholder = "Password";
+
+    let ip4 = document.createElement("input");
+    ip4.type = "text";
+    ip4.setAttribute("id", "username");
+    ip4.placeholder = "User_name";
+
+    let ip5 = document.createElement("input");
+    ip5.type = "text";
+    ip5.setAttribute("id", "mobile");
+    ip5.placeholder = "Mobile_No";
+
+    let ip6 = document.createElement("input");
+    ip6.type = "submit";
+    ip6.style.cursor = "pointer";
+    ip6.style.fontSize = "16px";
+
+    form.append(ip1, ip2, ip3, ip4, ip5, ip6)
+
+    div.append(form);
+
+    let divLogin = document.createElement("div");
+    divLogin.setAttribute("class", "divLogin");
+    let opt = document.createElement("P");
+    opt.textContent = "OR";
+
+    let loginButton = document.createElement("button");
+    loginButton.textContent = "Login";
+    loginButton.onclick = function(){
+        login(event);
+    }
+
+    divLogin.append(opt, loginButton);
+    div.append(divLogin);
+
+    parrent.append(div);
+
+    div.classList.add("active");
+}
+// User Registration
+function register(e){
+    e.preventDefault();
+
+    let form = document.getElementById("myLogInForm");
+
+    let obj = {
+        name: form.name.value,
+        email: form.email.value,
+        password: form.password.value,
+        username: form.username.value,
+        mobile: form.mobile.value,
+        description: "Defaul"
+    }
+    console.log("Data: ", obj);
+    obj = JSON.stringify(obj);
+
+    fetch("https://masai-api-mocker.herokuapp.com/auth/register", {
+
+            method: "POST",
+
+            body: obj,
+
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+
+        .then((res) => {
+            return res.json();
+        })
+
+        .then((res) => {
+            // console.log("Res_Suc: ", res);
+            alert(res.message)
+            if(res.error == false){
+                login(e, res.error);
+            }
+        })
+        .catch((err) => {
+            // console.log("error: ", err);
+            alert(err.message);
+        })
+
+}
+// login form
+function login(e, flag){
+
+    e.preventDefault();
+    let parrent = document.getElementById("mainContainer");
+
+    if(document.getElementById("loginForm") == null){
+        var div = document.createElement("div");
+        div.setAttribute("id", "loginForm");
+    }else{
+        var div = document.getElementById("loginForm");
+        div.innerHTML = null;
+    }
+
+    let img = document.createElement("img");
+    img.src = "https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/35/000000/external-close-banking-and-finance-kiranshastry-gradient-kiranshastry.png";
+    img.style.cursor = "pointer";
+    img.onclick = function(){
+        removeActive();
+    }
+    div.append(img);
+
+    if(flag === false){
+        let p = document.createElement("p");
+        p.textContent = "Registration success, please login now."
+        p.style.margin = "10px 0 0 0";
+        div.append(p);
+
+        setTimeout(() => {
+            p.style.display = "none";
+        },3000);
+    }
+
+    let heading = document.createElement("h3");
+    heading.textContent = "Login";
+    heading.style.textAlign = "center";
+    heading.style.margin = "10px 0 0 0";
+    div.append(heading);
+
+    let form = document.createElement("form");
+    form.setAttribute("id", "myLogInForm");
+    form.onsubmit = function(){
+        fetchUser(event);
+    }
+
+    let ip1 = document.createElement("input");
+    ip1.type = "text";
+    ip1.setAttribute("id", "username");
+    ip1.placeholder = "User_name";
+
+    let ip2 = document.createElement("input");
+    ip2.type = "text";
+    ip2.setAttribute("id", "password");
+    ip2.placeholder = "Password";
+
+    let ip3 = document.createElement("input");
+    ip3.type = "submit";
+    ip3.style.cursor = "pointer";
+    ip3.style.fontSize = "16px";
+
+    form.append(ip1, ip2, ip3)
+
+    div.append(form);
+
+    let divLogin = document.createElement("div");
+    divLogin.setAttribute("class", "divLogin");
+    let opt = document.createElement("P");
+    opt.textContent = "OR";
+
+    let loginButton = document.createElement("button");
+    loginButton.textContent = "Signup";
+    loginButton.onclick = function(){
+        loginPopup(event);
+    }
+
+    divLogin.append(opt, loginButton);
+    div.append(divLogin);
+
+    parrent.append(div);
+
+    div.classList.add("active");
+}
+// Login Authentica
+function fetchUser(e){
+    e.preventDefault();
+    let form = document.getElementById("myLogInForm");
+
+    let obj = {
+        password: form.password.value,
+        username: form.username.value,
+    }
+    console.log("Data: ", obj);
+
+    let objSend = JSON.stringify(obj);
+
+    fetch("https://masai-api-mocker.herokuapp.com/auth/login", {
+
+            method: "POST",
+
+            body: objSend,
+
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+
+        .then((res) => {
+            return res.json();
+        })
+
+        .then((res) => {
+            // console.log("Res: ", res);
+            if(res.error === true){
+                alert(res.message);
+            }else{
+                fetchUserData(obj.username, res.token);
+            }
+        })
+        .catch((err) => {
+            // console.log("error :", err);
+            alert(res.message)
+        })
+}
+// fetch login user data
+function fetchUserData(username, token){
+    fetch(`https://masai-api-mocker.herokuapp.com/user/${username}`, {
+
+            headers: {
+                "Content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        })
+
+        .then((res) => {
+            return res.json();
+        })
+
+        .then((res) => {
+            // console.log("Res_data: ", res, token);
+            if(token !== undefined){
+                showUserStatus(res.name);
+            }
+        })
+        .catch((err) => {
+            // console.log("error: ", err);
+            // alert(res.message)
+        })
+}
+// Dispaly user name in login status
+function showUserStatus(name){
+    let userStatus = document.getElementById("userStatus");
+    userStatus.innerHTML = null;
+
+    let p = document.createElement("p");
+    p.textContent = name;
+
+    userStatus.append(p);
+    removeActive();
+}
+
+function removeActive(){
+    let el = document.getElementById("loginForm");
+    el.innerHTML = null;
+    el.classList.remove("active");
+}
